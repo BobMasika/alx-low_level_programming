e347a76c1de7e599cca0b6bfd2f77c28b146261f
BobMasika <bob.masika@students.jkuat.ac.ke>
2022-09-08 16:17:18 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main-Ent point
+ *
+ *print- print string in quotes
+ *
+ * return 0: success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
