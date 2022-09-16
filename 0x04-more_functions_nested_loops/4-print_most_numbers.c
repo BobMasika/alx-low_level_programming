c269b91f16d114f5ea2e0cfeff69b3f4fd016e1f
BobMasika <bob.masika@students.jkuat.ac.ke>
2022-09-15 19:11:30 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - print numbers from 0 to 9
+ * except 2 and 4
+ *
+ * return 0
+ */
+void print_most_numbers(void)
+{
+	char n;
+
+	for (n = 48; n < 58; n++)
+	{
+		if ((n == 50) || (n == 52))
+		{
+			continue
+		}
+		_putchar(n)
+	}
+	putchar(10);
+}
