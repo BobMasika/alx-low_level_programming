91ef4653b9c896e866cca468eb6e33ef1e88953f
BobMasika <bob.masika@students.jkuat.ac.ke>
2022-09-15 18:19:42 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper -checks if a character is uppercase or not
+ * @c: character to be tested
+ * Return: 1 for uppercase, 0 for otherwise
+ */
+
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'z'))
+	{
+		return (1);
+	}
+
+	return (0);
+}
