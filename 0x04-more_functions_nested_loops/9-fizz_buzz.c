600f74adc11b33b24d1c74523c3be2be8119e9b4
BobMasika <bob.masika@students.jkuat.ac.ke>
2022-09-16 18:18:34 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,39 +10,25 @@ int main(void)
 {
 	int num = 1;
 
-	while (num++ < 100)
+	while (num <= 100)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
+		if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz ");
+		else if (num % 5 == 0)
 		{
-			printf("Fizzbuzz");
-		}
-		else if ((num % 3) == 0)
-		{
-			printf("Fizz ");
-		}
-		else if ((num % 5) == 0)
-		{
-			if (num != 100)
+			if (num == 100)
 			{
 				printf("Buzz");
 				printf("\n");
 			}
 			else
-			{
-				printf("Buzz");
-				printf("\n");
-			}
+				printf("Buzz ");
 		}
 		else if (num % 3 == 0)
-		{
-			printf("Fizz");
-			printf("\n");
-		}
+			printf("Fizz ");
 		else
-		{
 			printf("%d ", num);
-			num++;
-		}
-		return (0);
+		num++;
 	}
+	return (0);
 }
