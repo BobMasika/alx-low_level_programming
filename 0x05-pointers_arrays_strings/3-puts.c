04538e925c557f0e4030431afceada90c76f6be9
BobMasika <bob.masika@students.jkuat.ac.ke>
2022-09-19 17:07:30 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts -prints a string followed ny a new line
+ *
+ * @str: string to be printed
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	char *b;
+	int n;
+
+	b = str;
+
+	for (n = 0; b[n]; n++)
+	{
+		_putchar(b[n]);
+	}
+	_putchar('\n');
+}
